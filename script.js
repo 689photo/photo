e949172bd52b584698c0defeb8211f59fee6ed9f
689coding <86118109+689photo@users.noreply.github.com>
2024-03-12 00:08:11 +0900

    renew

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const overlay = document.getElementById('overlay');
+
+  setTimeout(() => {
+    overlay.style.opacity = '0';
+    overlay.addEventListener('transitionend', () => {
+      overlay.style.display = 'none';
+    });
+  }, 3000); // 3초 후 페이드 아웃
+
   const images = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg', 'photo7.jpg'];
   let currentIndex = 0;
 
-  // 이미지 미리 로드
   images.forEach((image) => {
     const img = new Image();
     img.src = image;
   });
 
   const currentPhoto = document.getElementById('currentPhoto');
-  currentPhoto.style.opacity = '1'; // 초기 사진이 바로 보이도록 설정
+  currentPhoto.style.opacity = '1';
 
   function changePhoto() {
     currentPhoto.style.opacity = '0';
@@ -18,8 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
       currentIndex = (currentIndex + 1) % images.length;
       currentPhoto.src = images[currentIndex];
       currentPhoto.style.opacity = '1';
-    }, 1000); // 흐릿하게 등장하는 트랜지션 시간
+    }, 1000);
   }
 
-  setInterval(changePhoto, 5000); // 사진 변경 주기
+  setInterval(changePhoto, 5000);
 });
