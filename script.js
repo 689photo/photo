15d6f1f3cc8dcedea6acbb81f47f0eb60ae6dd0c
689coding <86118109+689photo@users.noreply.github.com>
2024-03-12 12:13:33 +0900

    darkMode Transition

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // 문서 로드 완료 후 실행되는 초기화 함수
 document.addEventListener('DOMContentLoaded', function () {
-  const overlay = document.getElementById('overlay'); // 오버레이 요소 선택
-  const body = document.querySelector('body'); // body 요소 선택
-  const currentPhoto = document.getElementById('currentPhoto'); // 현재 사진 표시 요소 선택
+  const overlay = document.getElementById('overlay');
+  const body = document.querySelector('body');
+  const currentPhoto = document.getElementById('currentPhoto');
   let darkMode = false; // 다크 모드 상태 저장 변수
   const lightImages = [
-    // 라이트 모드에서 사용할 이미지 배열
     'photo1.jpg',
     'photo2.jpg',
     'photo3.jpg',
@@ -14,28 +13,33 @@ document.addEventListener('DOMContentLoaded', function () {
     'photo6.jpg',
     'photo7.jpg',
   ];
-  const darkImages = [
-    // 다크 모드에서 사용할 이미지 배열
-    'black1.jpg',
-    'black2.jpg',
-    'black3.jpg',
-    'black4.jpg',
-    'black5.jpg',
-  ];
-  let currentIndex = 0; // 현재 표시 중인 이미지의 인덱스
+  const darkImages = ['black1.jpg', 'black2.jpg', 'black3.jpg', 'black4.jpg', 'black5.jpg'];
 
-  // 오버레이 사라지는 로직
+  // 현재 표시 중인 이미지의 인덱스를 모드별로 저장
+  let currentIndex = 0;
+  let lastIndexLight = 0;
+  let lastIndexDark = 0;
+
   setTimeout(() => {
     overlay.style.opacity = '0';
     overlay.addEventListener('transitionend', () => (overlay.style.display = 'none'));
   }, 3000);
 
-  // 이미지 클릭 시 다크 모드 토글
+  // 이미지 클릭 시 다크 모드 토글 및 현재 사진 인덱스 저장/복원
   currentPhoto.addEventListener('click', function () {
+    if (darkMode) {
+      lastIndexDark = currentIndex; // 다크 모드를 떠나면서 현재 인덱스 저장
+      currentIndex = lastIndexLight; // 라이트 모드로 전환 시 마지막 인덱스 복원
+    } else {
+      lastIndexLight = currentIndex; // 라이트 모드를 떠나면서 현재 인덱스 저장
+      currentIndex = lastIndexDark; // 다크 모드로 전환 시 마지막 인덱스 복원
+    }
     darkMode = !darkMode;
+
+    // 다크 모드에 따른 배경색과 글자색 업데이트
     body.style.backgroundColor = darkMode ? 'black' : '';
     body.style.color = darkMode ? 'white' : '';
-    currentIndex = 0;
+
     updatePhoto();
   });
 
